7e4d3fa76dcec3f7f1d8826291285d472d4f21e2
TyGwyn <tygwyn2014@gmail.com>
2023-06-23 16:56:09 +0300

    выполнили задания 7-го семинара и домашней работы!//это занятие было для меня достаточно сложным, и я пользовался подсказками

--- Sem7Task49.py
+++ Sem7Task49.py
@@ -1,5 +1,5 @@
 orbits = [(1, 3), (2.5, 10), (7, 2), (6, 6), (4, 3)]
-s = list(map(lambda a:(a[0]!=a[1])* 3.14  *a[0]* a[1], orbits))
+s = list(map(lambda a:(a[0]!=a[1])* 3.14  * a[0]* a[1], orbits))
 print(max(orbits, key = lambda a:(a[0]!=a[1])* 3.14 * a[0] * a[1]))
 
 
